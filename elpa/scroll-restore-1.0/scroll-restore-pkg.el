dac668721a6b756203bf8b4a3fd613624cfa084b
Kseol <krendec@gmail.com>
2016-01-24 13:00:15 +0300

    first

---
+++ elpa/scroll-restore-1.0/scroll-restore-pkg.el
@@ -0,0 +1,1 @@
+(define-package "scroll-restore" "1.0" "restore original position after scrolling" 'nil :url "http://elpa.gnu.org/packages/scroll-restore.html" :keywords '("scrolling"))
---
+++ ksol-25.12-theme.el
@@ -0,0 +1,13 @@
+(deftheme ksol-25.12
+  "Created 2015-12-25.")
+
+(custom-theme-set-variables
+ 'ksol-25.12
+ '(custom-safe-themes (quote ("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "561ba4316ba42fe75bc07a907647caa55fc883749ee4f8f280a29516525fc9e8" default))))
+
+(custom-theme-set-faces
+ 'ksol-25.12
+ '(variable-pitch ((t (:family "Ubuntu Mono"))))
+ '(default ((t (:inherit nil :stipple nil :background "#000000" :foreground "light blue" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 151 :width normal :foundry "unknown" :family "Ubuntu Mono")))))
+
+(provide-theme 'ksol-25.12)
