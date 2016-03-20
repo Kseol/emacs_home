fb4f3dd605925ea93a6c48f72bda3b70c4322ff5
Kseol <krendec@gmail.com>
2016-03-21 02:21:35 +0300

    beforeiphyton

---
+++ elpa/zones-20160209.920/zones-pkg.el
@@ -0,0 +1,1 @@
+(define-package "zones" "20160209.920" "Zones of text - like multiple regions" 'nil :url "http://www.emacswiki.org/zones.el" :keywords '("narrow" "restriction" "widen" "region" "zone"))
