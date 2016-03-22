33c25ba0e7d7e3df6e4b1ecac7115bd8e77643a0
Kseol <krendec@gmail.com>
2016-03-22 11:43:23 +0300

    wokratwork

---
+++ elpa/concurrent-20160109.2046/concurrent-autoloads.el
@@ -0,0 +1,15 @@
+;;; concurrent-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("concurrent.el") (22255 48522 437572 942000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; concurrent-autoloads.el ends here
